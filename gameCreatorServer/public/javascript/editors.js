20e2e9d2a0032dd4a6bbc537007c583a4449c186
Emil Christiansen <emilsen68@gmail.com>
2021-11-09 14:31:39 +0100

    Server can now create weapon assets

--- gameCreatorServer/public/javascript/editors.js
+++ gameCreatorServer/public/javascript/editors.js
@@ -1,6 +1,20 @@
 function editors() {
+    var asset = {}
+    const weaponsEditor = document.getElementById("weaponsEditor")
+    const addWeapon = document.getElementById("addWeapon")
 
-    weaponsEditor = document.getElementById("weaponsEditor")
+    function showAsset() {
+        const textContainer = document.getElementById("jsonTextContainer")
+        textContainer.innerHTML = ""
+
+        const assetStringified = JSON.stringify(asset, null, 4)
+        textContainer.innerHTML = assetStringified
+
+        const data = "text/json;charset=utf-8," + encodeURIComponent(assetStringified);
+        const a = document.getElementById("downloadJSON")
+        a.href = 'data:' + data
+        a.download = 'asset.json'
+    }
 
     function activateWeapons() {
         console.log("Activated weapons")
@@ -12,15 +26,41 @@ function editors() {
         weaponsEditor.style = "display:none"
     }
 
-    funcs = {
+    const funcs = {
         "weapons": activateWeapons,
         "consumables": activateConsumables
     }
-    assetType = document.getElementById("assetType")
+    const assetType = document.getElementById("assetType")
     funcs[assetType.value]()
     
     assetType.addEventListener("change", (event) => {
         funcs[assetType.value]()
+    })
+
+    addWeapon.addEventListener("click", (event) => {
+        const weaponName = document.getElementById("weaponName").value
+        const weaponDesc = document.getElementById("weaponDesc").value
+        const dDiceMultiplier = document.getElementById("dDiceMultiplier").value
+        const dDiceType = document.getElementById("dDiceType").value
+        const hDiceMultiplier = document.getElementById("hDiceMultiplier").value
+        const hDiceType = document.getElementById("hDiceType").value
+        const weaponDT = document.getElementById("weaponDT").value
+        const weaponWeight = document.getElementById("weaponWeight").value
+
+
+        if (!asset.hasOwnProperty("weapons")) {
+            console.log("No weapons added previously")
+            asset["weapons"] = {}
+        }
+        asset["weapons"][weaponName] = {
+            desc: weaponDesc,
+            damageDice: `${dDiceType}x${dDiceMultiplier}`,
+            hitDice: `${hDiceType}x${hDiceMultiplier}`,
+            damageType: weaponDT,
+            weight: weaponWeight
+        }
+        console.log(asset)
+        showAsset()
     })
 
 }editors()
